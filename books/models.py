958197450ee8106631428f4feb97f328fae8e119
Ambar Cano <ambarcano@gmail.com>
2022-05-09 16:58:41 -0400

    partial goal 1 done

--- books/models.py
+++ books/models.py
@@ -8,3 +8,12 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Book(models.Model):
+    title       = models.CharField(max_length=455)
+    author      = models.CharField(max_length=455)
+    description = models.TextField(max_length=1000, null=True, blank=True)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    patternlnk  = models.URLField(blank=True,null=True)
+
--- books/views.py
+++ books/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
 # Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "books/list_books.html", 
+                 {"books": books})
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, "books/book_detail.html",       
+                    {"book": book})
+
